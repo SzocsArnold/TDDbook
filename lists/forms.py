f8c24977e86a6c1e0b904a7dec82ec1bdba82cf2
SzocsArnold <arnoldszocs96@gmail.com>
2019-02-07 22:31:57 +0200

    clean project so every test passes

--- lists/forms.py
+++ lists/forms.py
@@ -14,6 +14,11 @@ class ItemForm(forms.models.ModelForm):
             'text': forms.fields.TextInput(attrs={
                 'placeholder': 'Enter a to-do item',
                 'class': 'form-control input-lg',
+                'id':'text',
             }),
         }
         error_messages = {'text':{'required': EMPTY_ITEM_ERROR}}
+    
+    def save(self, for_list):
+        self.instance.list = for_list
+        return super().save()        
