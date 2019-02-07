76201cc1698d7789daaacc6a8e127daaa672eb53
SzocsArnold <arnoldszocs96@gmail.com>
2019-02-07 18:35:50 +0200

    newfolder

---
+++ functional_tests/base.py
@@ -0,0 +1,37 @@
+import os
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+import time
+
+
+
+
+class FunctionalTest(StaticLiveServerTestCase):
+
+    MAX_WAIT = 10
+   
+
+    def setUp(self):
+       self.browser = webdriver.Firefox(executable_path="C:\\Users\\Arnold\\geckodriver\\geckodriver.exe")
+
+    
+    
+    def wait_for_row_in_list_table(self, row_text):
+        
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > self.MAX_WAIT:
+                    raise e
+                time.sleep(0.5)    
+
+    
+        
+
+       
