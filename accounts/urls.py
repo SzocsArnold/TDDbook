8f07a03631d31c46bb9bb107fe90821d5b165bae
SzocsArnold <arnoldszocs96@gmail.com>
2019-02-13 22:32:00 +0200

    Custom passwordless auth backend + custom user model

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
 from accounts import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^send_login_email$', views.send_login_email, name='send_login_email'),
     url(r'^login$',views.login,name='login'),
-
+    url(r'^logout$', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
